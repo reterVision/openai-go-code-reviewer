742e56e11576472417b96a5b216bd6367de573f6
Chao Gao <reterclose@gmail.com>
2023-02-02 23:57:31 +0800

    use input argument

--- .github/scripts/hello.py
+++ .github/scripts/hello.py
@@ -1,6 +1,12 @@
+import sys
 import os
 
-repo_name = os.getenv("GIT_REPO")
-pr_number = os.getenv("PR_NUMBER")
+n = len(sys.argv)
+print("Total arguments passed:", n)
+
+if n != 3:
+    raise Exception("wrong input argument")
+
+repo_name, pr_number = sys.argv[1], sys.argv[2]
 
 print("Path to PR is %s/pulls/%s" % (repo_name, pr_number))
