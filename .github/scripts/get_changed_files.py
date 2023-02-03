49c91473ee775aebfbfdb1e0670b0b126dcdad50
Chao Gao <reterclose@gmail.com>
2023-02-03 17:04:57 +0800

    test new file

--- .github/scripts/get_changed_files.py
+++ .github/scripts/get_changed_files.py
@@ -51,7 +51,7 @@ def review_contents(filename, content, repo_name, pr_number, github_api_key, com
         "body": comments,
         "commit_id": commit_id,
         "path": filename,
-        "start_line":1, "start_side": "RIGHT", "line":2, "side": "RIGHT"
+        "start_line":1, "start_side": "RIGHT", "line":1, "side": "RIGHT"
     }
     print(f"Github request body: {request_body}")
 
@@ -64,7 +64,7 @@ def review_contents(filename, content, repo_name, pr_number, github_api_key, com
         },
         json=request_body
     )
-    print("Github comment response: %s" % response)
+    print("Github comment response: %s" % response.json())
 
 
 def get_review_comments(content):
